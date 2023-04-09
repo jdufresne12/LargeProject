8db2d346d6083224b518183a3b166c0537ed3d7e
John Dufresne <jdufresne135@gmail.com>
2023-04-09 18:38:27 -0400

    React-Native app initialized

---
+++ app/index.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const Home = () => {
+  return (
+    <View>
+      <Text>Welcome to our homepage</Text>
+    </View>
+  );
+};
+
+export default Home;
