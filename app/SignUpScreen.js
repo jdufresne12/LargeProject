2df9f3af41bfc8dd2b516698afe71236b9439bf0
Reynaldo-Martinez2021 <89822899+Reynaldo-Martinez2021@users.noreply.github.com>
2023-04-25 15:42:36 -0400

    Update SignUpScreen.js

--- app/SignUpScreen.js
+++ app/SignUpScreen.js
@@ -98,7 +98,7 @@ const SignUpScreen = () => {
 
   const signUpUser = async (userName, password) => {
     try {
-      const response = await fetch("http://167.99.248.180/api/signup", {
+      const response = await fetch("http://159.223.108.232/api/signup", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
